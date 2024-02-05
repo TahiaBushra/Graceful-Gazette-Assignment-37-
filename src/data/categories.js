365b030fcd767a8de2ff41c979d1cc31cde210c2
TahiaBushra <bushratahia@gmail.com>
2024-02-05 23:41:11 +0600

    Categories almost done

---
+++ src/data/categories.js
@@ -0,0 +1,70 @@
+export const categories = [
+  {
+    id: 1,
+    title: "Category 1",
+    description:
+      "Fashion, a dynamic realm of creativity and self-expression, embraces clothing, accessories, and evolving styles influenced by culture and individual tastes. It's a vibrant fusion where personal expression converges with cultural influences.",
+    banner:
+      "https://images.pexels.com/photos/5706336/pexels-photo-5706336.jpeg?auto=compress&cs=tinysrgb&w=600",
+    url: "/category-1",
+    items: [
+      {
+        id: 1,
+        image: "",
+        title: "",
+        description: "",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Category 2",
+    description:
+      "Embark on a journey in fashion, where self-expression meets artistry. From timeless classics to avant-garde trends, immerse yourself in a realm where fabrics, colors, and designs intertwine to craft a vibrant tapestry of style.",
+    banner:
+      "https://images.pexels.com/photos/6044231/pexels-photo-6044231.jpeg?auto=compress&cs=tinysrgb&w=600",
+    url: "/category-2",
+    items: [
+      {
+        id: 2,
+        image: "",
+        title: "",
+        description: "",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Category 3",
+    description:
+      "Fashion, a dynamic realm of self-expression, celebrates diversity and innovation. From timeless classics to avant-garde trends, it's a canvas for individual style, reflecting the evolving dialogue between culture, identity, and aesthetics.",
+    banner:
+      "https://images.pexels.com/photos/2766334/pexels-photo-2766334.jpeg?auto=compress&cs=tinysrgb&w=600",
+    url: "/category-3",
+    items: [
+      {
+        id: 3,
+        image: "",
+        title: "",
+        description: "",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Category 4",
+    description:
+      "Explore fashion's dynamic world, where style meets expression. From timeless classics to avant-garde creations, delve into a kaleidoscope of trends. Discover the artistry behind clothing, accessories, and personal adornment in this ever-evolving landscape.",
+    banner:
+      "https://images.pexels.com/photos/7747109/pexels-photo-7747109.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    url: "/category-4",
+    items: [
+      {
+        id: 4,
+        image: "",
+        title: "",
+        description: "",
+      },
+    ],
+  },
+];
--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,7 +1,34 @@
+import { Link } from "react-router-dom";
+import Overlay from "../components/Overlay";
+import { categories } from "../data/categories";
+import { useState } from "react";
+
 const Categories = () => {
+  const [expandCategory, setExpandCategory] = useState(0);
   return (
-    <div>
-      <p></p>
+    <div className="w-screen h-screen flex">
+      {categories.map((category, index) => (
+        <Link
+          onMouseOver={() => setExpandCategory(index)}
+          key={category.id}
+          className={`h-full ${
+            index === expandCategory ? "w-[calc(100vw-60vw)]" : "w-[20vw]"
+          } overflow-hidden relative`}
+        >
+          <img
+            src={category.banner}
+            alt={category.title}
+            className="w-full h-full object-cover"
+          />
+          <Overlay />
+          <div className="z-[2] absolute top-0 right-0 bottom-0 w-full h-full p-10 flex flex-col gap-5 items-center justify-center text-center text-slate-50">
+            <h2 className="text-4xl font-semibold antialiased">
+              {category.title}
+            </h2>
+            <p className="hidden">{category.description}</p>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 };
