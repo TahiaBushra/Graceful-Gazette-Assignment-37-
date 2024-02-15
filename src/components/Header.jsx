9fb7a14f0fb31432e4a5102aff6241e2d8db0be7
TahiaBushra <bushratahia@gmail.com>
2024-02-15 22:58:42 +0600

    Done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -42,7 +42,7 @@ const Header = () => {
             <div className="flex backdrop-blur-lg rounded-full border overflow-hidden bg">
               <div className="rounded-2xl w-52 h-auto bg-gray-900 cursor-pointer">
                 <img
-                  src="/src/assets/images/New_Project-removebg-preview (1).png"
+                  src="../assets/images/New_Project-removebg-preview (1).png"
                   alt="fashion pic"
                   className="w-full h-full object-cover hover:scale-110 transition duration-300 ease-in-out"
                 />
